1092a50fc3fa9e92d85cd8077a7045d9a1fb6829
RyanFurt12 <ryanfurtadinho9@gmail.com>
2024-08-18 23:18:25 -0300

    fix: configuring brightness util

--- src/services/utils.jsx
+++ src/services/utils.jsx
@@ -4,7 +4,7 @@ function isColorLight(color) {
 	const g = parseInt(color.substring(2, 4), 16);
 	const b = parseInt(color.substring(4, 6), 16);
 	const brightness = (r * 299 + g * 587 + b * 114) / 1000;
-	return brightness > 180;
+	return brightness > 160;
 }
 
 export {isColorLight};
